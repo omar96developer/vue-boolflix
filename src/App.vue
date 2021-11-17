23ea930dce579b0b35b434fc1eba9004066f8df3
omar96developer <omakino@hotmail.it>
2021-11-17 16:13:47 +0100

    milestone 1 finish

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,53 @@
 <template>
   <div id="app">
-  
+    <div>
+      <input type="text" placeholder="Ricerca..." v-model="query" @keyup.enter="film()">
+      <button @click="film()">cerca</button>
+      <div></div>
+    </div>
+    <div>
+      <ul>
+        <li v-for="(movie, id) in movies" :key="id">
+          {{movie.title}}, {{movie.original_title}}, {{movie.original_language}}, {{movie.vote_average}}
+          
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 /* import HelloWorld from './components/HelloWorld.vue' */
-//import axios from "axios"
+import axios from "axios"
 
 export default {
   name: 'App',
   components: {
+    
+  },
+  data(){
+    return{
+      apiUrl: 'https://api.themoviedb.org/3',
+      apiKey: '9caf6d244b3195b2f3c44cba59c630ef',
+      movies: undefined,
+      query: '',
+    }
+  },
+  methods:{
+    film() {
+      axios.get(this.apiUrl + '/search/movie', {
+      params: {
+        api_key: this.apiKey,
+        query: this.query,
+      }
+      }).then(resp =>{
+        this.movies = resp.data.results;
+      });
+      this.query = ''
+      return this.movies
+    }
+  },
+  mounted() {
     
   }
 }
@@ -21,7 +58,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  
  
 }
 </style>
